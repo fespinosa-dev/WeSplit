5d38677c28aeb4a817825de9a228ff765db25bc3
Fernando Espinosa <fernando.espinosa@episodesix.com>
2022-07-03 18:10:19 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Fernando Espinosa on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
